24e521ccf4d4bbd3f3cf39ab5290ec22c88138f7
Evgeniy Dobrunov <e.dobrunov@gmail.com>
2023-07-08 20:41:45 +0300

    add bool fullButton for 2 variants of button states

--- lib/main_screen/widgets/add_shoes_widget.dart
+++ lib/main_screen/widgets/add_shoes_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 import 'add_shoes_button.dart';
@@ -11,10 +12,16 @@ class AddShoesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        AddShoesButton(
+        // AddShoesButton(
+        //   width: MediaQuery.of(context).size.width * 0.9,
+        //   height: MediaQuery.of(context).size.width * 0.2,
+        // ),
+        AddShoesFullButton(
           width: MediaQuery.of(context).size.width * 0.9,
           height: MediaQuery.of(context).size.width * 0.2,
+          fullButton: false,
         ),
+
         const SizedBox(height: 15),
         Padding(
           padding: const EdgeInsets.only(bottom: 15.0),
@@ -41,6 +48,40 @@ class AddShoesWidget extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class AddShoesFullButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final bool fullButton;
+
+  const AddShoesFullButton({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.fullButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: const Radius.circular(20),
+      color: fullButton ? Colors.transparent : const Color.fromRGBO(29, 185, 221, 0.32),
+      strokeWidth: 2,
+      dashPattern: const [10, 6],
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(0, 0, 0, 0.2),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child:
+            fullButton ? const SizedBox() : const Icon(Icons.add, color: Color.fromRGBO(29, 185, 221, 1.00), size: 32),
+      ),
     );
   }
 }
